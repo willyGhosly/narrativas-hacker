6a84f492a7ed62e36598d88ffd8fcba739c8977b
Ghostly <willgonzalez2017@gmail.com>
2022-11-19 21:25:31 -0500

    Add accordeon script

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+accordeon()
+function accordeon() {
+    const blocks = document.querySelectorAll('.accordeon__block')
+    const contents = document.querySelectorAll('.accordeon__content')
+    if (blocks) {
+        blocks.forEach(block => {
+            const barToClick = block.querySelector('.accordeon__title')
+            const content = block.querySelector('.accordeon__content')
+            barToClick.onclick = () => {
+                contents.forEach(c => [
+                    c.classList.remove('active')
+                ])
+                content.classList.add('active')
+            }
+        })
+    }
+}
