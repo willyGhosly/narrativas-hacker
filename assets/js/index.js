839cf0e3114c5ada2ff40c7a4815fe93aa775fe7
Ghostly <willgonzalez2017@gmail.com>
2022-11-23 12:41:34 -0500

    adding conection between accordeon script and css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,54 @@
 'use strict'
 
-accordeon()
+function jump(hash) {
+    let url = location.href
+    location.href = "#" + hash
+    history.replaceState(null, null, url)
+}
+
 function accordeon() {
     const blocks = document.querySelectorAll('.accordeon__block')
     const contents = document.querySelectorAll('.accordeon__content')
+    const navBarItems = document.querySelectorAll('.header__link--accordeon')
     if (blocks && contents) {
-        blocks.forEach(block => {
+        blocks.forEach((block, index) => {
             const barToClick = block.querySelector('.accordeon__title')
             const content = block.querySelector('.accordeon__content')
             barToClick.onclick = () => {
-                contents.forEach(c => [
-                    c.classList.remove('active')
-                ])
-                content.classList.add('active')
+                if (content.classList.contains('active')) {
+                    content.classList.remove('active')
+                } else {
+                    contents.forEach(content => {
+                        content.classList.remove('active')
+                    })
+                    content.classList.add('active')
+                    setTimeout(() => {
+                        jump(`home_acc_block--${index + 1}`)
+                    }, 100)
+                }
             }
         })
     }
+    // if (navBarItems && contents) {
+    //     navBarItems.forEach((item, index) => {
+    //         item.onclick = (e) => {
+    //             e.preventDefault()
+    //             console.log(e)
+    //             if (contents[index].classList.contains('active')) {
+    //                 contents[index].classList.remove('active')
+    //             } else {
+    //                 contents.forEach(content => {
+    //                     content.classList.remove('active')
+    //                 })
+    //                 contents[index].classList.add('active')
+    //                 setTimeout(() => {
+    //                     jump(`home_acc_block--${index + 1}`)
+    //                 }, 100)
+    //             }
+    //         }
+    //     })
+    // }
 }
+
+/** Load Functions */
+accordeon()
